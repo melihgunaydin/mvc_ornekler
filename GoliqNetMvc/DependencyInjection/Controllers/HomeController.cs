ce3859e3209c1eaf7d6a77daaabf7e4982e8205c
Melih Günaydın <melihgunaydin28@gmail.com>
2020-08-04 10:32:01 +0300

    first commit

---
+++ GoliqNetMvc/DependencyInjection/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DependencyInjection.Models.Loggers;
+
+namespace DependencyInjection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ILogger _logger;
+
+        public HomeController(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public ActionResult Index()
+        {
+            //var logger = new DatabaseLogger();
+            //logger.Log();
+
+            _logger.Log();
+
+            return Content("Index");
+        }
+    }
+}
